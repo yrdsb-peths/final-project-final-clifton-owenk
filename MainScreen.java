96466cb4158c717b9ba2ec2928610cc603423c95
OwenKung0124 <440002825@gapps.yrdsb.ca>
2025-05-25 16:41:04 -0400

    Add start button sound

--- MainScreen.java
+++ MainScreen.java
@@ -6,19 +6,22 @@ import greenfoot.*;
  * guide
  * upgrade
  */
-public class MainScreen extends World {
+public class MainScreen extends World
+{
+    private boolean started = false; //check if game started
     public MainScreen() {
         super(400, 750, 1); // size of main screen
         
         GreenfootImage bg = new GreenfootImage("mainScreen_background.png");
         bg.scale(getWidth(), getHeight()); //resize background to world size
         setBackground(bg);
+        
+        // add start button, near buttom of main screen
+        addObject(new StartButton(), getWidth() / 2, getHeight() - 100); 
     }
 
-    public void act() {
-        // Start game when user presses a key (e.g. space)
-        if (Greenfoot.isKeyDown("space")) {
-            Greenfoot.setWorld(new GameWorld()); // Change to your actual game world
-        }
+    public void act() 
+    {
+
     }
 }
---
+++ StartButton.java
@@ -0,0 +1,30 @@
+import greenfoot.*;
+
+/**
+ * StartButton is button on main screen
+ * If clicked by mouse then jump to gameworld 
+ */
+public class StartButton extends Actor {
+    private GreenfootImage normalImage = new GreenfootImage("start.png");
+    private GreenfootImage pressedImage = new GreenfootImage("start1.png");
+
+    public StartButton() 
+    {
+        normalImage.scale(250, 370); //size of button
+        pressedImage.scale(250, 350); 
+        setImage(normalImage);
+    }
+
+    public void act() {
+        if (Greenfoot.mousePressed(this)) 
+        {
+            setImage(pressedImage); // Change to pressed version
+        }
+
+        if (Greenfoot.mouseClicked(this)) 
+        {
+            Greenfoot.playSound("start.mp3"); //play sound when clicked
+            Greenfoot.setWorld(new GameWorld()); // Start the game
+        }
+    }
+}
